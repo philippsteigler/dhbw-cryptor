b0dfaad473448d0dbbc7d7fd9c61b57f927fc8d5
roman.wuertemberger@gmail.com <Goltenhof74>
2019-03-19 17:06:07 +0100

    Grundlage AES

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,48 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+
 public class Controller {
+
+    private File document;
+    private File picture;
+
+    @FXML Label label_picturePath;
+    @FXML Label label_filePath;
+
+    //Öffnet eine neie Scene für die Dateiauswahl
+    //Speichert die Datei in die Variable document
+    public void loadFile() {
+        FileChooser fileChooser = new FileChooser();
+
+        document = fileChooser.showOpenDialog(new Stage());
+
+        label_filePath.setText(document.getPath());
+    }
+
+    //Öffnet eine neie Scene für die Bildauswahl
+    //Speichert das Bild in die Variable picture
+    public void loadPicture() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.jpg"));
+
+        picture = fileChooser.showOpenDialog(new Stage());
+
+        label_picturePath.setText(picture.getPath());
+    }
+
+    //Verschlüsseln und Verstecken der Datei
+    public void encrypt() throws Exception {
+        FileInputStream fileInputStream = new FileInputStream(document);
+
+        byte[] inputBytes = new byte[(int) document.length()];
+    }
+
 }
