d3d6cb1d4ecefbe921cdcc57faeea961848d8fd9
roman.wuertemberger@gmail.com <Goltenhof74>
2019-03-24 16:13:40 +0100

    User in datei speichern

--- src/sample/UserAdministration.java
+++ src/sample/UserAdministration.java
@@ -1,7 +1,13 @@
 package sample;
 
+import org.omg.IOP.Encoding;
+
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -10,17 +16,31 @@ public class UserAdministration {
 
     ArrayList<User> users;
 
+    String filename = "Users.txt";
+
     public UserAdministration() {
         users = new ArrayList<>();
+        try {
+            setUsers();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void setUsers() throws IOException, ClassNotFoundException {
-        FileInputStream fileInputStream = new FileInputStream("Test.txt");
-        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-        this.users = (ArrayList<User>) (objectInputStream.readObject());
-        objectInputStream.close();
+    public void setUsers() throws IOException{
+        byte[] encoded = Files.readAllBytes(Paths.get("Users.txt"));
+        String usersString =  new String(encoded, "UTF-8");
 
-        System.out.println(this.users);
+        String[] userStrings = usersString.split("/");
+
+        for (String userString: userStrings) {
+            userString.trim();
+            String[] attributes = userString.split(",");
+            if(attributes.length == 3) {
+                users.add(new User(Integer.parseInt(attributes[0]), attributes[1], attributes[2].getBytes(Charset.forName("UTF-8"))));
+            }
+        }
     }
 
     public void createUser(int id, String name, String key) {
@@ -41,10 +61,17 @@ public class UserAdministration {
         String usersString = "";
 
         for (User user: users) {
-            usersString = usersString + "(" + Integer.toString(user.getId()) + ", " + user.getName() + ", " + ByteBuffer.wrap(user.getKey()) + ")";
+            usersString = usersString + Integer.toString(user.getId()) + "," + user.getName() + "," + user.getKey() + "/";
+            usersString.trim();
         }
 
-        PrintWriter out = new PrintWriter("filename.txt");
-        out.println(usersString);
+        PrintWriter printWriter = new PrintWriter(filename);
+        printWriter.write(usersString);
+        printWriter.close();
+        printWriter.flush();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(filename);
+        fileOutputStream.write(usersString.getBytes());
+        fileOutputStream.close();
     }
 }
