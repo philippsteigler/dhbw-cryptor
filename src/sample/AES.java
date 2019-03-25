c9e32b742c2231725be516ee4e20b9673b62fd18
squanchy11 <philipp.steigler@mailbox.org>
2019-03-25 17:44:14 +0100

    Diffie Hellman zum sicheren Schlüsselaustausch. Zur verwendung beim Anlegen neuer User.

--- src/sample/AES.java
+++ src/sample/AES.java
@@ -2,61 +2,31 @@ package sample;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Base64;
 
 class AES {
 
-    private static SecretKeySpec secretKey;
-
-    private static void setKey(String myKey)
-    {
-        MessageDigest sha;
+    static byte[] encrypt(byte[] inputBytes, byte[] secret) {
         try {
-            byte[] key = myKey.getBytes("UTF-8");
-            sha = MessageDigest.getInstance("SHA-1");
-            key = sha.digest(key);
-            key = Arrays.copyOf(key, 16);
-            secretKey = new SecretKeySpec(key, "AES");
-        }
-        catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static byte[] encrypt(byte[] inputBytes, String secret)
-    {
-        try
-        {
-            setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            cipher.init(Cipher.ENCRYPT_MODE,  new SecretKeySpec(secret, 0, 16, "AES"));
             return cipher.doFinal(inputBytes);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Error while encrypting: " + e.toString());
         }
+
         return null;
     }
 
 
-    static byte[] decrypt(byte[] toDecrypt, String secret)
-    {
-        try
-        {
-            setKey(secret);
+    static byte[] decrypt(byte[] toDecrypt, byte[] secret) {
+        try {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(secret, 0, 16, "AES"));
             return cipher.doFinal(toDecrypt);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Error while decrypting: " + e.toString());
         }
+
         return null;
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Arrays;
+
 public class Main extends Application {
 
     @Override
