49541dc30e5e26f7a2065ba66c3b6120e4e60f6d
squanchy11 <philipp.steigler@mailbox.org>
2019-03-25 17:45:17 +0100

    Fixed Import

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,12 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Arrays;
-
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Cryptor");
         primaryStage.setScene(new Scene(root));
